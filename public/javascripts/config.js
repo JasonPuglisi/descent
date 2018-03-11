425a88fddf55d8179dd9b9651823f45e86966f98
Jason Puglisi <hello@jasonpuglisi.com>
2018-03-10 20:05:50 -0500

    Added options to unblur background and specify default background image

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -13,6 +13,18 @@ function processBackground() {
     $(this).addClass('selected');
     Cookies.set('background', this.id.substring(this.id.lastIndexOf('-') + 1), { expires: 3650 });
   });
+
+  selected = !cookieExists('blur') || cookieEnabled('blur');
+  $('#select-group-bgopt-blur').toggleClass('selected', selected).on('click', function() {
+    selected = !selected;
+    $(this).toggleClass('selected');
+    Cookies.set('blur', selected, { expires: 365 });
+  });
+
+  $('#defaultBackground').val(Cookies.get('defaultBackground'));
+  $('#defaultBackground').on('input propertychange paste', function() {
+    $('#defaultBackgroundSave').show();
+  });
 }
 
 function processUnits() {
@@ -30,8 +42,7 @@ function processDatetime() {
   let options = ['24hr', 'weekday', 'seconds'];
   options.forEach(option => {
     let selected = cookieEnabled(option);
-    if (selected)
-      $(`#select-group-datetime-${option}`).addClass('selected');
+    $(`#select-group-datetime-${option}`).toggleClass('selected', selected);
 
     $(`#select-group-datetime-${option}`).on('click', function() {
       selected = !selected;
@@ -39,4 +50,10 @@ function processDatetime() {
       Cookies.set(option, selected, { expires: 3650 });
     });
   });
+}
+
+function saveDefaultBackground() {
+  $('#defaultBackgroundSave').hide();
+
+  Cookies.set('defaultBackground', $('#defaultBackground').val(), { expires: 3650 });
 }
