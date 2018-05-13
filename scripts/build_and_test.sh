9c9a4f1aecdf9b60fa5091b11318ff5941401501
Jason Puglisi <hello@jasonpuglisi.com>
2018-05-12 20:25:33 -0400

    Fixed build/deployment for static files

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-echo "Starting build"
-echo "Starting gulp global install"
+echo -e "\e[33mStarting build\e[39m"
+echo -e "\e[33mStarting gulp global install\e[39m"
 yarn global add gulp
-echo "Finished gulp global install"
-echo "Starting gulp build"
+echo -e "\e[32mFinished gulp global install\e[39m"
+echo -e "\e[33mStarting gulp build\e[39m"
 gulp build
-echo "Finished gulp build"
-echo "Finished build"
-echo "Tests not implemented"
-echo "Starting file clean for deployment"
-rm -rf node_modules
-echo "Finished file clean for deployment"
-echo "Ready to deploy"
+echo -e "\e[32mFinished gulp build\e[39m"
+echo -e "\e[32mFinished build\e[39m"
+echo -e "\e[34mTests not implemented\e[39m"
+echo -e "\e[34mReady to deploy\e[39m"
