cc171b8d5950d49f4a8b0aec597692ea00255a7a
Jason Puglisi <hello@jasonpuglisi.com>
2022-09-06 22:15:15 -0700

    Node fetch migration (#123)
    * Migrate Spotify functions to node-fetch
    
    * Migrate weather functions to node-fetch
    
    * Migrate hue functions to node-fetch
    
    * Add semicolon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
-const express = require('express');
-const request = require('request');
+import bodyParser from 'body-parser';
+import crypto from 'crypto';
+import express from 'express';
+import fetch from 'node-fetch';
 
 /* Base application functionality */
 
@@ -148,7 +148,7 @@ app.get('/now/app/hue/authorize', (req, res) => {
   });
 });
 
-function authenticateHue(code, refreshToken, callback) {
+async function authenticateHue(code, refreshToken, callback) {
   if (!code && !refreshToken) {
     console.warn('Error authenticating with Hue: No authorization code or refresh token');
     callback();
@@ -164,43 +164,50 @@ function authenticateHue(code, refreshToken, callback) {
   }
 
   let url = code ? `https://api.meethue.com/oauth2/token?code=${code}&grant_type=authorization_code` : 'https://api.meethue.com/oauth2/refresh?grant_type=refresh_token';
-  request.post(url, (err, res) => {
-    if (err || res.statusCode !== 401) {
-      console.warn('Error authenticating with Hue: No initial challenge');
-      callback();
-      return;
-    }
 
-    let authHeader = res.headers['www-authenticate'];
-    let realm = authHeader.match(/realm="(.+?)"/)[1];
-    let nonce = authHeader.match(/nonce="(.+?)"/)[1];
-    let uriSuffix = code ? 'token' : 'refresh';
-    let digest = calculateHueDigest(clientId, clientSecret, realm, nonce, uriSuffix);
+  const response = await fetch(url, {
+    'method': 'post'
+  });
 
-    let auth = `Digest username="${clientId}", realm="${realm}", nonce="${nonce}", uri="/oauth2/${uriSuffix}", response="${digest}"`;
-    let headers = { 'Authorization': auth };
+  if (!response.ok && response.status != 401) {
+    console.warn('Error authenticating with Hue: No initial challenge');
+    callback();
+    return;
+  }
 
-    let form = code ? undefined : { refresh_token: refreshToken };
+  let authHeader = response.headers.get('www-authenticate');
+  let realm = authHeader.match(/realm="(.+?)"/)[1];
+  let nonce = authHeader.match(/nonce="(.+?)"/)[1];
+  let uriSuffix = code ? 'token' : 'refresh';
+  let digest = calculateHueDigest(clientId, clientSecret, realm, nonce, uriSuffix);
 
-    request.post(url, { headers, form }, (err, res, body) => {
-      if (err || res.statusCode !== 200) {
-        console.warn(`Error authenticating with Hue: Digest failure: ${body}`);
-        callback();
-        return;
-      }
+  let auth = `Digest username="${clientId}", realm="${realm}", nonce="${nonce}", uri="/oauth2/${uriSuffix}", response="${digest}"`;
+  let headers = { 'Authorization': auth };
 
-      let data = JSON.parse(body);
-      let accessToken = data.access_token;
-      let accessTokenExpiry = (parseInt(data.access_token_expires_in) - 300) * 1000;
-      let refreshToken = data.refresh_token;
-      let refreshTokenExpiry = (parseInt(data.refresh_token_expires_in) - 300) * 1000;
-      let tokenType = data.token_type;
+  let form = code ? undefined : { refresh_token: refreshToken };
 
-      hueWhitelistApplication(accessToken, (username) => {
-        let auth = { accessToken, accessTokenExpiry, refreshToken, refreshTokenExpiry, tokenType, username };
-        callback(auth);
-      });
-    });
+  const response2 = await fetch(url, {
+    'method': 'post',
+    'body': form,
+    'headers': headers
+  });
+
+  if (!response2.ok) {
+    console.warn(`Error authenticating with Hue: Digest failure`);
+    callback();
+    return;
+  }
+
+  let data = await response2.json();
+  let accessToken = data.access_token;
+  let accessTokenExpiry = (parseInt(data.access_token_expires_in) - 300) * 1000;
+  let refreshTokenNew = data.refresh_token;
+  let refreshTokenExpiry = (parseInt(data.refresh_token_expires_in) - 300) * 1000;
+  let tokenType = data.token_type;
+
+  hueWhitelistApplication(accessToken, (username) => {
+    let auth = { accessToken, accessTokenExpiry, refreshTokenNew, refreshTokenExpiry, tokenType, username };
+    callback(auth);
   });
 }
 
@@ -217,34 +224,46 @@ function calculateHueDigest(clientId, clientSecret, realm, nonce, uriSuffix) {
   return digest;
 }
 
-function hueWhitelistApplication(accessToken, callback) {
+async function hueWhitelistApplication(accessToken, callback) {
   let url = 'https://api.meethue.com/bridge/0/config';
   let headers = { 'Authorization': `Bearer ${accessToken}`, 'Content-Type': 'application/json' };
   let body = JSON.stringify({ linkbutton: true });
-  request.put(url, { headers, body }, (err, res, body) => {
-    url = 'https://api.meethue.com/bridge/';
-    body = JSON.stringify({ devicetype: 'Descent' });
 
-    request.post(url, { headers, body }, (err, res, body) => {
-      let data = JSON.parse(body);
-      callback(data[0].success.username);
-    });
+  const response = await fetch(url, {
+    'method': 'put',
+    'body': body,
+    'headers': headers
   });
+
+  url = 'https://api.meethue.com/bridge/';
+  body = JSON.stringify({ devicetype: 'Descent' });
+
+  const response2 = await fetch(url, {
+    'method': 'post',
+    'body': body,
+    'headers': headers
+  });
+
+  let data = await response2.json();
+  callback(data[0].success.username);
 }
 
-app.post('/now/app/hue/api/groups', (req, res) => {
+app.post('/now/app/hue/api/groups', async (req, res) => {
   let accessToken = req.body.accessToken;
   let username = req.body.username;
 
   let url = `https://api.meethue.com/bridge/${username}/groups`;
   let headers = { 'Authorization': `Bearer ${accessToken}` };
-  request(url, { headers }, (err, res2, body) => {
-    let data = JSON.parse(body);
-    res.json(data);
+
+  const response = await fetch(url, {
+    'headers': headers
   });
+
+  let data = await response.json();
+  res.json(data);
 });
 
-app.post('/now/app/hue/api/light', (req, res) => {
+app.post('/now/app/hue/api/light', async (req, res) => {
   let accessToken = req.body.accessToken;
   let username = req.body.username;
 
@@ -255,10 +274,15 @@ app.post('/now/app/hue/api/light', (req, res) => {
   let url = `https://api.meethue.com/bridge/${username}/lights/${id}/state`;
   let headers = { 'Authorization': `Bearer ${accessToken}`, 'Content-Type': 'application/json' };
   let body = `{"xy": [${colorX},${colorY}]}`;
-  request.put(url, { headers, body }, (err, res2, body) => {
-    let data = JSON.parse(body);
-    res.json(data);
+
+  const response = await fetch(url, {
+    'method': 'put',
+    'body': body,
+    'headers': headers
   });
+
+  let data = await response.json();
+  res.json(data);
 });
 
 /* Spotify functionality */
@@ -275,36 +299,40 @@ class Artist {
   }
 }
 
-function authenticateSpotify(client, secret) {
+async function authenticateSpotify(client, secret) {
   if (!client || !secret) {
     console.warn('Error getting Spotify authorization: No API credentials');
     return;
   }
 
   let authorization = Buffer.from(`${client}:${secret}`).toString('base64');
-  let options = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': `Basic ${authorization}`
-    },
-    body: 'grant_type=client_credentials'
+  let url = 'https://accounts.spotify.com/api/token';
+  let body = 'grant_type=client_credentials';
+  let headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': `Basic ${authorization}`
   };
-  request.post(options, (err, res, body) => {
-    if (err || res.statusCode != 200) {
-      console.warn(`Error getting Spotify authorization: ${err}`);
-      spotifyKey = null;
-      setTimeout(() => { authenticateSpotify(client, secret); }, 1800000);
-      return;
-    }
 
-    let data = JSON.parse(body);
-    spotifyKey = data.access_token;
-    setTimeout(() => { authenticateSpotify(client, secret); }, data.expires_in * 1000);
+  const response = await fetch(url, {
+    'method': 'post',
+    'body': body,
+    'headers': headers
   });
+
+  if (!response.ok) {
+    console.warn(`Error getting Spotify authorization: ${response.statusText}`);
+    spotifyKey = null;
+    setTimeout(() => { authenticateSpotify(client, secret); }, 1800000);
+    return;
+  }
+
+  const data = await response.json();
+
+  spotifyKey = data.access_token;
+  setTimeout(() => { authenticateSpotify(client, secret); }, data.expires_in * 1000);
 }
 
-app.post('/now/app/spotify/track', (req, res) => {
+app.post('/now/app/spotify/track', async (req, res) => {
   if (!spotifyKey) {
     console.warn('Error getting Spotify track: No API key');
     res.json(new Track());
@@ -314,35 +342,36 @@ app.post('/now/app/spotify/track', (req, res) => {
   let artist = encodeURIComponent(req.body.artist);
   let title = encodeURIComponent(req.body.title);
   let query = `${artist}%20-%20${title}`;
-
-  let options = {
-    url: `https://api.spotify.com/v1/search?q=${query}&type=track&limit=1`,
-    headers: {
-      'Authorization': `Bearer ${spotifyKey}`
-    }
+  let url = `https://api.spotify.com/v1/search?q=${query}&type=track&limit=1`;
+  let headers = {
+    'Authorization': `Bearer ${spotifyKey}`
   };
-  request(options, (err, res2, body) => {
-    if (err || res2.statusCode != 200) {
-      console.warn(`Error getting Spotify track: Invalid response: ${err}`);
-      res.json(new Track());
-      return;
-    }
 
-    let data = JSON.parse(body);
-    if (data.tracks.total < 1) {
-      console.warn('Error getting Spotify track: No results');
-      res.json(new Track());
-      return;
-    }
-
-    let track = data.tracks.items[0];
-    track.success = true;
-
-    res.json(track);
+  const response = await fetch(url, {
+    'headers': headers
   });
+
+  if (!response.ok) {
+    console.warn(`Error getting Spotify track: Invalid response: ${err}`);
+    res.json(new Track());
+    return;
+  }
+
+  const data = await response.json();
+
+  if (data.tracks.total < 1) {
+    console.warn('Error getting Spotify track: No results');
+    res.json(new Track());
+    return;
+  }
+
+  let track = data.tracks.items[0];
+  track.success = true;
+
+  res.json(track);
 });
 
-app.post('/now/app/spotify/artist', (req, res) => {
+app.post('/now/app/spotify/artist', async (req, res) => {
   if (!spotifyKey) {
     console.warn('Error getting Spotify artist: No API key');
     res.json(new Artist());
@@ -351,26 +380,26 @@ app.post('/now/app/spotify/artist', (req, res) => {
 
   let artist = encodeURIComponent(decodeURIComponent(req.body.artist));
   let query = artist;
-
-  let options = {
-    url: `https://api.spotify.com/v1/artists/${query}`,
-    headers: {
-      'Authorization': `Bearer ${spotifyKey}`
-    }
+  let url = `https://api.spotify.com/v1/artists/${query}`;
+  let headers = {
+    'Authorization': `Bearer ${spotifyKey}`
   };
-  request(options, (err, res2, body) => {
-    if (err || res2.statusCode != 200) {
-      console.warn(`Error getting Spotify artist: Invalid response: ${err}`);
-      res.json(new Artist());
-      return;
-    }
 
-    let data = JSON.parse(body);
-    let artist = data;
-    artist.success = true;
-
-    res.json(artist);
+  const response = await fetch(url, {
+    'headers': headers
   });
+
+  if (!response.ok) {
+    console.warn(`Error getting Spotify artist: Invalid response: ${err}`);
+    res.json(new Artist());
+    return;
+  }
+
+  let data = await response.json();
+
+  data.success = true;
+
+  res.json(data);
 });
 
 /* Weather (OpenWeatherMap and DarkSky) functionality */
@@ -409,94 +438,96 @@ app.post('/now/app/weather', (req, res) => {
   }
 });
 
-function getWeatherDarkSky(key, lat, lon, units, callback) {
+async function getWeatherDarkSky(key, lat, lon, units, callback) {
   units = units === 'imperial' ? 'us' : 'si';
   let urlLat = encodeURIComponent(lat);
   let urlLon = encodeURIComponent(lon);
   let url = `https://api.darksky.net/forecast/${key}/${urlLat},${urlLon}?units=${units}`;
 
-  request(url, (err, res, body) => {
-    if (err || res.statusCode != 200)
-      callback(`Invalid response: ${err}`, new Weather());
+  const response = await fetch(url);
 
-    let data = JSON.parse(body);
-    let icons = {
-      'clear-day': 'day-sunny',
-      'clear-night': 'night-clear',
-      'rain': 'rain',
-      'snow': 'snow',
-      'sleet': 'sleet',
-      'wind': 'cloudy-gusts',
-      'fog': 'fog',
-      'cloudy': 'cloudy',
-      'partly-cloudy-day': 'day-cloudy',
-      'partly-cloudy-night': 'night-alt-cloudy',
-      'hail': 'hail',
-      'thunderstorm': 'thunderstorm',
-      'tornado': 'tornado'
-    };
+  if (!response.ok) {
+    callback(`Invalid response: ${err}`, new Weather());
+  }
 
-    let weather = new Weather(true);
-    weather.summary = data.minutely.summary;
-    weather.temperature = Math.round(data.currently.temperature);
-    weather.apparentTemperature = Math.round(data.currently.apparentTemperature);
-    weather.unit = units === 'us' ? 'F' : 'C';
-    weather.icon = icons[data.currently.icon];
+  let data = await response.json();
+  let icons = {
+    'clear-day': 'day-sunny',
+    'clear-night': 'night-clear',
+    'rain': 'rain',
+    'snow': 'snow',
+    'sleet': 'sleet',
+    'wind': 'cloudy-gusts',
+    'fog': 'fog',
+    'cloudy': 'cloudy',
+    'partly-cloudy-day': 'day-cloudy',
+    'partly-cloudy-night': 'night-alt-cloudy',
+    'hail': 'hail',
+    'thunderstorm': 'thunderstorm',
+    'tornado': 'tornado'
+  };
 
-    callback(null, weather);
-  });
+  let weather = new Weather(true);
+  weather.summary = data.minutely.summary;
+  weather.temperature = Math.round(data.currently.temperature);
+  weather.apparentTemperature = Math.round(data.currently.apparentTemperature);
+  weather.unit = units === 'us' ? 'F' : 'C';
+  weather.icon = icons[data.currently.icon];
+
+  callback(null, weather);
 }
 
-function getWeatherOpenweathermap(key, lat, lon, units, callback) {
+async function getWeatherOpenweathermap(key, lat, lon, units, callback) {
   let urlLat = encodeURIComponent(lat);
   let urlLon = encodeURIComponent(lon);
   let urlUnits = encodeURIComponent(units);
   let url = `http://api.openweathermap.org/data/2.5/weather?lat=${urlLat}&lon=${urlLon}&units=${urlUnits}&appid=${key}`;
 
-  request(url, (err, res, body) => {
-    if (err || res.statusCode != 200)
-      return callback(`Invalid response: ${err}`, new Weather());
+  const response = await fetch(url);
 
-    let data = JSON.parse(body);
+  if (!response.ok) {
+    return callback(`Invalid response: ${err}`, new Weather());
+  }
 
-    let icons = {
-      '01d': 'day-sunny',
-      '01n': 'night-clear',
-      '02d': 'day-cloudy',
-      '02n': 'night-cloudy',
-      '03d': 'cloud',
-      '03n': 'cloud',
-      '04d': 'cloudy',
-      '04n': 'cloudy',
-      '09d': 'rain',
-      '09n': 'rain',
-      '10d': 'day-rain',
-      '10n': 'night-rain',
-      '11d': 'thunderstorm',
-      '11n': 'thunderstorm',
-      '13d': 'snow',
-      '13n': 'snow',
-      '50d': 'windy',
-      '50n': 'windy'
-    };
-    let apparent;
-    if (units === 'imperial')
-      apparent = 35.74 + 0.6215 * data.main.temp - 35.75 * Math.pow(data.wind.speed, 0.16)
-        + 0.4275 * data.main.temp * Math.pow(data.wind.speed, 0.16);
-    else
-      apparent = 13.12 + 0.6215 * data.main.temp - 11.37 * Math.pow(data.wind.speed, 0.16)
-        + 0.3965 * data.main.temp * Math.pow(data.wind.speed, 0.16);
+  let data = await response.json();
 
-    let weather = new Weather(true);
-    weather.summary = `${data.weather[0].description} currently`;
-    weather.summary = `${weather.summary.substring(0, 1).toUpperCase()}${weather.summary.substring(1)}`;
-    weather.temperature = Math.round(data.main.temp);
-    weather.apparentTemperature = Math.round(apparent);
-    weather.unit = units === 'imperial' ? 'F' : 'C';
-    weather.icon = `wi wi-${icons[data.weather[0].icon]}`;
+  let icons = {
+    '01d': 'day-sunny',
+    '01n': 'night-clear',
+    '02d': 'day-cloudy',
+    '02n': 'night-cloudy',
+    '03d': 'cloud',
+    '03n': 'cloud',
+    '04d': 'cloudy',
+    '04n': 'cloudy',
+    '09d': 'rain',
+    '09n': 'rain',
+    '10d': 'day-rain',
+    '10n': 'night-rain',
+    '11d': 'thunderstorm',
+    '11n': 'thunderstorm',
+    '13d': 'snow',
+    '13n': 'snow',
+    '50d': 'windy',
+    '50n': 'windy'
+  };
+  let apparent;
+  if (units === 'imperial')
+    apparent = 35.74 + 0.6215 * data.main.temp - 35.75 * Math.pow(data.wind.speed, 0.16)
+      + 0.4275 * data.main.temp * Math.pow(data.wind.speed, 0.16);
+  else
+    apparent = 13.12 + 0.6215 * data.main.temp - 11.37 * Math.pow(data.wind.speed, 0.16)
+      + 0.3965 * data.main.temp * Math.pow(data.wind.speed, 0.16);
 
-    callback(null, weather);
-  });
+  let weather = new Weather(true);
+  weather.summary = `${data.weather[0].description} currently`;
+  weather.summary = `${weather.summary.substring(0, 1).toUpperCase()}${weather.summary.substring(1)}`;
+  weather.temperature = Math.round(data.main.temp);
+  weather.apparentTemperature = Math.round(apparent);
+  weather.unit = units === 'imperial' ? 'F' : 'C';
+  weather.icon = `wi wi-${icons[data.weather[0].icon]}`;
+
+  callback(null, weather);
 }
 
 /* Application runtime */
